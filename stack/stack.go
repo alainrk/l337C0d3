6e8deb9e14fa2518497da9806598d76925ec3c80
Alain <alain.devcs@gmail.com>
2022-04-18 15:04:25 +0200

    stack

---
+++ stack/stack.go
@@ -0,0 +1,25 @@
+package l337C0d3
+
+type Stack struct {
+	size  int
+	Items []interface{}
+}
+
+func (s *Stack) Push(item interface{}) {
+	s.size++
+	s.Items = append(s.Items, item)
+}
+
+func (s *Stack) Pop() (interface{}, bool) {
+	if s.size == 0 {
+		return nil, false
+	}
+	item := s.Items[len(s.Items)-1]
+	s.Items = s.Items[:len(s.Items)-1]
+	s.size--
+	return item, true
+}
+
+func (s Stack) IsEmpty() bool {
+	return s.size == 0
+}
