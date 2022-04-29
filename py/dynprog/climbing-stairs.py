0c4d15b49bbf2dd23b87f574867de9895ad8dda0
Alain <alain.devcs@gmail.com>
2022-04-29 08:54:33 +0200

    stairs - better

--- py/dynprog/climbing-stairs.py
+++ py/dynprog/climbing-stairs.py
@@ -14,7 +14,28 @@ def climbStairs(n):
     m.append(m[i-1] + m[i-2])
   return m[-1]
 
+def climbStairs2(n):
+  '''
+  1 = 1
+  2 = 1+1, 2
+  3 = 1+1+1, 2+1, 1+2
+  4 = 1+1+1+1, 2+1+1, 1+2+1, 1+1+2, 2+2
+
+  ways(i) = ways(i-1) + ways(i-2)
+  '''
+  m = [1, 2]
+  if n <= 2:
+    return m[n-1]
+  for i in range (3,n+1):
+    m[0], m[1] = m[1], m[0] + m[1]
+  return m[1]
+
 assert(climbStairs(1) == 1)
 assert(climbStairs(2) == 2)
 assert(climbStairs(3) == 3)
 assert(climbStairs(4) == 5)
+
+assert(climbStairs2(1) == 1)
+assert(climbStairs2(2) == 2)
+assert(climbStairs2(3) == 3)
+assert(climbStairs2(4) == 5)
