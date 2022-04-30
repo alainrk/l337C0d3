74a74ec45ce806b56d2ebd96e0d868c4068629a4
Alain <alain.devcs@gmail.com>
2022-04-30 07:51:08 +0200

    log to explain

--- py/interval/insert.py
+++ py/interval/insert.py
@@ -1,20 +1,28 @@
+hline = '\n' + ('-' * 40) + '\n'
+
 def insert(intervals, n):
+  print(f"{hline}Solving for intervals: {intervals}, {n}")
   res = []
   i = 0 # needed after loop as well
   for i, (x, y) in enumerate(intervals):
+    print(f"\n-Loop i = {i}, x,y = ({x},{y})")
     # newInterval is after current one
     if n[0] > y:
+      print(f"n[0] > y [{n[0]} > {y}] -- add [{x}, {y}] to result")
       res.append([x, y])
     # newInterval intersec current one, exit loop and merge pieces
     elif n[1] < x:
+      print(f"n[1] < x [{n[1]} < {x}] -- decrement i to {i-1}")
       i -= 1
       break
     # set the newInterval using the current values and go on
     else:
       n[0] = min(n[0], x)
       n[1] = max(n[1], y)
+      print(f"set new interval and go on...")
   # concat res so far, the new interval and the rest of the intervals
   res += [n] + intervals[i+1:]
+  print(f"result = {res} + {[n]} + {intervals[i+1:]}")
   return res
 
 assert(insert([[1, 3], [3, 5]], [2, 4]) == [[1, 5]])
