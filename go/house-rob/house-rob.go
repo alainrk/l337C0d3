95c0cd6b8152648ccd8a52a9e50ff00e355aae57
Alain <alain.devcs@gmail.com>
2022-05-30 10:36:57 +0100

    house rob - iter

--- go/house-rob/house-rob.go
+++ go/house-rob/house-rob.go
@@ -1,5 +1,12 @@
 package l337C0d3
 
+func max(a, b int) int {
+	if a >= b {
+		return a
+	}
+	return b
+}
+
 func solveRob(nums []int, pos int, memo map[int]int) int {
 	if pos < 0 {
 		return 0
@@ -11,14 +18,20 @@ func solveRob(nums []int, pos int, memo map[int]int) int {
 	return memo[pos]
 }
 
+// Top down
 func rob(nums []int) int {
 	memo := map[int]int{}
 	return solveRob(nums, len(nums)-1, memo)
 }
 
-func max(a, b int) int {
-	if a >= b {
-		return a
+// Bottom up
+func robIter(nums []int) int {
+	if len(nums) == 1 {
+		return nums[0]
 	}
-	return b
+	res := [2]int{0, nums[0]}
+	for i := 1; i < len(nums); i++ {
+		res[0], res[1] = res[1], max(res[1], res[0]+nums[i])
+	}
+	return res[1]
 }
